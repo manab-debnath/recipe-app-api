bdc5fe79d0ee9dc95ba02d35f8b380a02e7228a5
manab-debnath <manab_debnath@outlook.com>
2025-06-21 02:06:16 +0530

    add cron.js

--- src/server.js
+++ src/server.js
@@ -4,9 +4,12 @@ import { ENV } from "./config/env.js";
 import { db } from "./config/db.js";
 import { favoritesTable } from "./db/schema.js";
 import { and, eq } from "drizzle-orm";
+import job from "./config/cron.js";
 
 const app = express();
 const port = ENV.PORT;
+
+if (ENV.NODE_ENV == "production") job.start();
 
 app.use(cors());
 app.use(express.json());
